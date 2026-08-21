eb2ea0cf92a014f0533d1c69e0b96520fe48a0d2
walkerj91 <jonwalker2123@gmail.com>
2026-08-21 21:57:24 +0100

    FP

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-import { supabase } from 'supabaseClient.js';
+import { supabase } from './supabaseClient.js';
 
 // ── DOM refs ──────────────────────────────────────────────
 const binderSelectionScreen = document.getElementById('binderSelectionScreen');
