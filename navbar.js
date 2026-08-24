9ed976eaf3b7661bdd4ee54e3e679063c59ede85
walkerj91 <jonwalker2123@gmail.com>
2026-08-24 22:35:38 +0100

    Refactor navbar.js for improved session handling

--- navbar.js
+++ navbar.js
@@ -58,8 +58,8 @@ export async function renderNavbar(containerId) {
 
             <div class="dropdown">
                 <div class="dropdwon-title-row">
-                    <a href="./marketplace.html">MARKETPLACE▾</a>
-                    <span class="dropdown-toggle" aria-label="Toggle Marketplace submenu"></span>
+                    <a href="./marketplace.html">MARKETPLACE</a>
+                    <span class="dropdown-toggle" aria-label="Toggle Marketplace submenu">▾</span>
                 </div>
                 <div class="dropdown-content">
                     <a href="./binders.html">Binders</a>
