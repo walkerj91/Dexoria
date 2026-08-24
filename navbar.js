49a8a90a2783d98c186f3cd727f3025f3c898bed
walkerj91 <jonwalker2123@gmail.com>
2026-08-24 23:11:00 +0100

    Refactor navbar.js to improve readability and structure

--- navbar.js
+++ navbar.js
@@ -58,8 +58,7 @@ export async function renderNavbar(containerId) {
 
             <div class="dropdown">
                 <div class="dropdwon-title-row">
-                    <a href="./marketplace.html">MARKETPLACE</a>
-                    <span class="dropdown-toggle" aria-label="Toggle Marketplace submenu">▾</span>
+                    <a href="./marketplace.html">MARKETPLACE▾</a>
                 
                 <div class="dropdown-content">
                     <a href="./binders.html">Binders</a>
@@ -71,9 +70,8 @@ export async function renderNavbar(containerId) {
 
             <div class="dropdown">
                 <div class="dropdown-title-row">
-                    <a href="./portfolio.html" id="portfolio-nav-link">PORTFOLIO</a>
-                    <span class="dropdown-toggle" aria-label="Toggle Portfolio submenu">▾</span>
-                
+                    <a href="./portfolio.html" id="portfolio-nav-link">PORTFOLIO▾</a>
+                                    
                 <div class="dropdown-content">
                     <a href="./set-tracker.html" id="set-tracker-link">Set Tracker</a>
                 </div>
