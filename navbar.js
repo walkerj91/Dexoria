8c90ceba00a87f73ab02d31ab7aac7908d9f93ea
walkerj91 <jonwalker2123@gmail.com>
2026-09-25 16:18:36 +0100

    Refactor navbar.js to improve session handling

--- navbar.js
+++ navbar.js
@@ -92,7 +92,7 @@ export async function renderNavbar(containerId) {
                     </button>
                 </div>
                 <div class="dropdown-content">
-                    <a href="./games.html">Games Corner</a>
+                    <a href="./games.html">Game Corner</a>
                 </div>
             </div>
 
